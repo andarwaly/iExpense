7bd243b666eea7b9a690a6a14b19357c92478b12
Dzaky Waly <andarwaly@gmail.com>
2024-10-15 14:25:43 +0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Muhammad Dzaky on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
